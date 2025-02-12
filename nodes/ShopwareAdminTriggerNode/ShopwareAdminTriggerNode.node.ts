eeed751df7d843e5855ebc5b8151897d379f982f
Johannes Berthel <j.berthel@codeblick.de>
2025-02-12 11:42:07 +0100

    feat: add trigger node

---
+++ nodes/ShopwareAdminTriggerNode/ShopwareAdminTriggerNode.node.ts
@@ -0,0 +1,173 @@
+import {
+	IDataObject,
+	IExecuteFunctions,
+	IHookFunctions,
+	ILoadOptionsFunctions,
+	IWebhookFunctions,
+	IWebhookResponseData,
+	NodeConnectionType,
+	type INodeType,
+	type INodeTypeDescription,
+} from 'n8n-workflow';
+import axios, { AxiosRequestConfig } from 'axios';
+
+export class ShopwareAdminTriggerNode implements INodeType {
+	description: INodeTypeDescription = {
+		name: 'shopwareAdminTriggerNode',
+		displayName: 'Shopware Trigger',
+		icon: 'file:shopware.svg',
+		group: ['trigger'],
+		version: 1,
+		description: 'Shopware Trigger',
+		defaults: {
+			name: 'Shopware Trigger',
+		},
+		// eslint-disable-next-line n8n-nodes-base/node-class-description-inputs-wrong-regular-node
+		inputs: [],
+		// eslint-disable-next-line n8n-nodes-base/node-class-description-outputs-wrong
+		outputs: [NodeConnectionType.Main],
+		credentials: [
+			{
+				name: 'shopwareAdminCredentialsApi',
+				required: true,
+			},
+		],
+		requestDefaults: {
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/json',
+			},
+			baseURL: '={{$credentials.url}}',
+		},
+		webhooks: [
+			{
+				name: 'default',
+				httpMethod: 'POST',
+				responseMode: 'onReceived',
+				path: 'webhook',
+			},
+		],
+		properties: [
+			{
+				displayName: 'Event Name',
+				name: 'eventName',
+				type: 'string',
+				default: '',
+				required: true,
+			},
+			{
+				displayName: 'Only Live Version',
+				name: 'onlyLiveVersion',
+				type: 'boolean',
+				default: true,
+			},
+		],
+	};
+
+	webhookMethods = {
+		default: {
+			async checkExists(this: IHookFunctions): Promise<boolean> {
+				const webhookData = this.getWorkflowStaticData('node');
+
+				if (webhookData.webhookId === undefined) {
+					return false;
+				}
+
+				try {
+					await axios({
+						...(await authenticate.call(this)),
+						url: `/webhook/${webhookData.webhookId}`,
+						method: 'GET',
+					});
+				} catch (error) {
+					return false;
+				}
+
+				return true;
+			},
+			async create(this: IHookFunctions): Promise<boolean> {
+				const webhookUrl = this.getNodeWebhookUrl('default');
+				const webhookData = this.getWorkflowStaticData('node');
+
+				const eventName = this.getNodeParameter('eventName') as string;
+				const onlyLiveVersion = this.getNodeParameter('onlyLiveVersion') as boolean;
+
+				try {
+					const result = await axios({
+						...(await authenticate.call(this)),
+						url: '/webhook',
+						method: 'POST',
+						data: {
+							name: `n8n-${this.getNode().id}`,
+							url: webhookUrl,
+							eventName,
+							onlyLiveVersion,
+						},
+					});
+
+					webhookData.webhookId = result.data.data.id;
+				} catch (error) {
+					return false;
+				}
+
+				return true;
+			},
+			async delete(this: IHookFunctions): Promise<boolean> {
+				const webhookData = this.getWorkflowStaticData('node');
+
+				try {
+					await axios({
+						...(await authenticate.call(this)),
+						url: `/webhook/${webhookData.webhookId}`,
+						method: 'DELETE',
+					});
+				} catch (error) {
+					return false;
+				}
+
+				delete webhookData.webhookId;
+
+				return true;
+			},
+		},
+	};
+
+	async webhook(this: IWebhookFunctions): Promise<IWebhookResponseData> {
+		const req = this.getRequestObject();
+
+		return {
+			workflowData: [this.helpers.returnJsonArray(req.body as IDataObject)],
+		};
+	}
+}
+
+async function authenticate(
+	this: IHookFunctions | IExecuteFunctions | ILoadOptionsFunctions | IWebhookFunctions,
+): Promise<AxiosRequestConfig> {
+	const credentials = await this.getCredentials('shopwareAdminCredentialsApi');
+
+	const axiosRequestConfig: AxiosRequestConfig = {
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		method: 'POST',
+		url: credentials.url + '/oauth/token',
+		data: {
+			grant_type: 'client_credentials',
+			client_id: credentials.clientId,
+			client_secret: credentials.clientSecret,
+		},
+	};
+
+	const result = await axios(axiosRequestConfig);
+
+	const { access_token } = result.data;
+
+	return {
+		headers: {
+			'Content-Type': 'application/json',
+			Authorization: `Bearer ${access_token}`,
+		},
+		baseURL: credentials.url as string,
+	};
+}
